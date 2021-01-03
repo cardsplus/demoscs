e1cd82b48e1fd6142c6b25ff7eb84f0c1d5d389c
Robert Bruckbauer <robert.bruckbauer@e-mundo.de>
2021-01-03 18:56:58 +0100

    Svelte-App

--- VERSION.md
+++ VERSION.md
@@ -1,4 +1,4 @@
-# Version 0.1
+# Version 0.1.0
 
 * Setup für `gradle`
 * Setup für `cardsplus`
---
+++ app/frontend-svelte/rollup.config.js
@@ -0,0 +1,108 @@
+import svelte from 'rollup-plugin-svelte';
+const smelte=require("smelte/rollup-plugin-smelte");
+import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
+import { config } from 'dotenv';
+
+const production = !process.env.ROLLUP_WATCH;
+
+function serve() {
+	let server;
+	
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+				stdio: ['ignore', 'inherit', 'inherit'],
+				shell: true
+			});
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		}
+	};
+}
+
+export default {
+	input: 'src/main/svelte/main.js',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'frontend',
+		file: 'public/build/bundle.js'
+	},
+	plugins: [
+		config(),
+		replace({
+			BACKEND_URL: () => process.env.BACKEND_URL
+				? JSON.stringify(process.env.BACKEND_URL)
+				: '"http://localhost:8080"',				
+			BACKEND_USER: () => process.env.BACKEND_USER
+			? JSON.stringify(process.env.BACKEND_USER)
+			: '"user:password"'
+        }),	  
+
+		svelte({
+			// enable run-time checks when not in production
+			dev: !production,
+			// we'll extract any component CSS out into
+			// a separate file - better for performance
+			css: css => {
+				css.write('bundle.css');
+			}
+		}),
+
+		smelte({ 
+			purge: production,
+			output: "public/index.css", 
+			postcss: [],
+			whitelist: [],
+			whitelistPatterns: [],
+			tailwind: { 
+				colors: { 
+					primary: "#b027b0",
+					secondary: "#009688",
+					error: "#f44336",
+					success: "#4caf50",
+					alert: "#ff9800",
+					blue: "#2196f3",
+					dark: "#212121" 
+			  	},
+				darkMode: false,
+			}
+		}),
+
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		resolve({
+			browser: true,
+			dedupe: ['svelte']
+		}),
+		commonjs(),
+
+		// In dev mode, call `npm run start` once
+		// the bundle has been generated
+		!production && serve(),
+
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
+		!production && livereload('public'),
+
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser()
+	],
+	watch: {
+		clearScreen: false
+	}
+};
