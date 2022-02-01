af356c6bf313bebb2e99710fafc14ce6ad34056b
robert.bruckbauer <robert.bruckbauer@e-mundo.de>
2022-02-01 11:49:45 +0100

    Vet

---
+++ app/client/src/main/svelte/pages/VetEditor.svelte
@@ -0,0 +1,79 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+	import Button from '../components/Button';
+	import TextField from '../components/TextField';
+    
+    export let visible = false;
+    export let vet = undefined;
+
+    let showUpdate;
+    let showRemove;
+    let newVet = {
+        name: undefined
+    }
+
+    $: disabled = !newVet.name;
+    $: if (vet) onChange()
+    function onChange() {
+        showUpdate = true;
+        showRemove = !vet.aktiv;
+        newVet = {
+            id: vet.id,
+            name: vet.name
+        }
+        console.log(['onChange', newVet]);
+    }
+
+    const dispatch = createEventDispatcher();
+    function onCreate() {
+        visible = false;
+        console.log(['create', newVet]);
+        dispatch('create', newVet);
+    }
+    function onUpdate() {
+        visible = false;
+        console.log(['update', newVet]);
+        dispatch('update', newVet);
+    }
+    function onRemove() {
+        visible = false;
+        console.log(['remove', newVet]);
+        dispatch('remove', newVet);
+    }
+    function onCancel() {
+        visible = false;
+    }
+</script>
+
+<div class="flex flex-col">
+    <div class="w-full">
+        <TextField bind:value={newVet.name} 
+            label="Name"		
+            placeholder="Bitte den Namen eingeben"/>
+    </div>
+</div>
+
+<div class="py-4">
+    {#if showUpdate}
+    <Button on:click={() => onUpdate()} {disabled}>
+        Ok
+    </Button>
+    {:else}
+    <Button on:click={() => onCreate()} {disabled}>
+        Ok
+    </Button>
+    {/if}
+    {#if showRemove}
+    <Button on:click={() => onRemove()}>
+        Löschen
+    </Button>
+    {/if}
+    <Button on:click={() => onCancel()}>
+        Abbrechen
+    </Button>
+</div>
+
+<details>
+    <summary>JSON</summary>
+    <pre>{JSON.stringify(newVet, null, 2)}</pre>
+</details>
---
+++ app/client/src/main/svelte/pages/VetViewer.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from "svelte";
+	import { toast } from '../components/Toast';
+    import { loadOneValue } from "../utils/rest.js";
+
+    export let id;
+
+    let vet = {
+        name: undefined
+    };
+
+    onMount(async () => {
+        try {
+            vet = await loadOneValue('/api/vet/' + id);
+            console.log(['onMount', vet]);
+        } catch(err) {
+			console.log(['onMount', err]);
+			toast.push(err.toString());
+        };
+    });
+</script>
+
+<h1>{vet.name}</h1>
